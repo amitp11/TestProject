c9894efddf204037c9e36b84f61221651bb41009
amitp11 <amitpathak7294@gmail.com>
2024-03-02 18:49:14 +0530

    added method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,45 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String a = "abc";
+        String b = "pqr";
+        //mergeAlternately(a,b);
         System.out.println("Hello World");
+        String str1 = "ABCABC", str2 = "ABC";
+        gcdOfStrings(str1,str2);
 
-        SubClass s = new SubClass();
-        s.setB(20);
-        s.getB();
-        s.setA(10);
-        s.getA();
+    }
 
+    private static void gcdOfStrings(String str1, String str2) {
+        HashSet<Character> set = new HashSet<>();
+        if(str1!=null && str2!=null){
+            for(Character c:str1.toCharArray()){
+                if(!set.contains(c))
+                set.add(c);
+                else break;
+            }
+        }
+        System.out.println(set.toString());
+    }
+
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        while (i < word1.length() || i < word2.length()) {
+            if (i < word1.length()) {
+                result.append(word1.charAt(i));
+            }
+            if (i < word2.length()) {
+                result.append(word2.charAt(i));
+            }
+            i++;
+        }
+        System.out.println(result.toString());
+        return result.toString();
     }
 }
