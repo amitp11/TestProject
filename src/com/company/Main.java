b42278f7f9a38b9b456dc5801956213187dac2f4
amitp11 <amitpathak7294@gmail.com>
2024-03-06 18:34:01 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class Main {
@@ -11,8 +12,30 @@ public class Main {
         //mergeAlternately(a,b);
         System.out.println("Hello World");
         String str1 = "ABCABC", str2 = "ABC";
-        gcdOfStrings(str1,str2);
+        //gcdOfStrings(str1,str2);
+        int[] a1 = {1,3},a2={2};
+        double d = findMedianSortedArrays(a1,a2);
+        System.out.printf("Value of D -> "+d);
 
+    }
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2){
+        int a = nums1.length, b= nums2.length;
+        int[] mergedArray = new int[a+b];
+        int k =0;
+        for(int i=0;i<a;i++)
+            mergedArray[k++] = nums1[i];
+        for(int i=0;i<b;i++)
+            mergedArray[k++] = nums2[i];
+        Arrays.sort(mergedArray);
+        int total = mergedArray.length;
+        if(total%2==1)
+            return (double) mergedArray[total/2];
+        else {
+            double m1 = mergedArray[total / 2];
+            double m2 = mergedArray[total / 2 - 1];
+            return ((double)m1+(double)m2)/2.0;
+        }
     }
 
     private static void gcdOfStrings(String str1, String str2) {
