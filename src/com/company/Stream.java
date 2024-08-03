2f0a2ea836e39c59fb46588da3c352a0f60795ff
amitp11 <amitpathak7294@gmail.com>
2024-08-03 20:30:42 +0530

    Create Stream.java

---
+++ src/com/company/Stream.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Stream {
+
+    public static void main(String[] args) {
+        String a = "abc";
+        String b = "pqr";
+        System.out.printf(a +" " +b);
+    }
+}
